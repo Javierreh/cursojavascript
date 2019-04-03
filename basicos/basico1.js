f410aeddbde5cffa87a6f5ac6bfd66ce9e56e94f
javierreh <javiarroyoherrera@gmail.com>
2019-04-03 16:46:10 +0200

    Clase 1 de javascript

---
+++ basicos/basico1.js
@@ -0,0 +1,42 @@
+// alert("Hola Mundo");
+
+var edadAlumno;
+const IMPUESTO_IVA = 0.21;
+
+alert(edadAlumno);
+
+edadAlumno = 35;
+
+alert(edadAlumno);
+
+var nombreAlumno = prompt("Dame un nombre");
+
+// var Attr = null;
+
+
+// string
+	var nombre = "Javi";
+	var apellido = "Arroyo";
+
+	var codigoHtml = '<div class="rojo">' + nombre + ' ' + apellido + '</div>';
+
+
+// number
+	var edad = 35;
+	var resultado = 34.5;
+	var negativos = -32;
+
+
+// boolean
+	var estado1 = true;
+	var estado2 = false;
+
+
+// null
+	var estadoNull = null;
+
+
+	alert(typeof(edad));
+
+	var nombre, apellido = "juan";
+	alert(apellido);
