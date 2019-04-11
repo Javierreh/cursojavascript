65d675cbcec5fd80c9ce18dd19b2520149d17882
javierreh <javiarroyoherrera@gmail.com>
2019-04-11 18:00:24 +0200

    Día 7 de JavaScript..

---
+++ DOM/js/imagen.js
@@ -0,0 +1,18 @@
+var imagen = document.getElementById('imagen');
+
+imagen.src = "images/gato.jpg";
+
+// imagen.style.width = "200px";
+// imagen.style.padding = "20px";
+// imagen.style.border = "1px solid black";
+// imagen.style.display = "block";
+// imagen.style.transform = "rotateZ(30deg)";
+// imagen.title = "holaaa a she ajaksksj";
+
+
+// alert(imagen.style.width);
+// alert(imagen.getAttribute('id'));
+
+imagen.setAttribute('style', 'background-color: red; padding: 10px');
+
+imagen.setAttribute('class', 'imagencita animacion blablabla');
