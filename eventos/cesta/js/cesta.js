821d0e9380a96ed1718b9ad633253028f4d7dcec
javierreh <javiarroyoherrera@gmail.com>
2019-04-14 13:17:33 +0200

    Juego de la cesta mejorado...

--- eventos/cesta/js/cesta.js
+++ eventos/cesta/js/cesta.js
@@ -2,34 +2,17 @@ var cesta = document.getElementById('cesta');
 var escenario = document.getElementById('escenario');
 var bolitas = document.getElementsByClassName('bolita');
 
+var scoreDisplay = document.getElementById('score');
+var hearts = document.getElementsByClassName('hearts');
+
+var heartsDiv = document.getElementById('heartsDiv');
+
 var cestaPositionLeft = 200;
-var lifes = 10;
+var lifes = 5;
 var score = 0;
 
-// 
-
-////////////////
-
-//version con el movimiento del raton
-
-// escenario.addEventListener('mousemove', moverCesta);
-
-// function moverCesta(event) {
-
-// 	var posicionX = event.layerX -= (cesta.clientWidth / 2);
-
-// 	cesta.style.left = posicionX + "px";
-
-// 	if (posicionX > (escenario.clientWidth - cesta.clientWidth) - 6) {
-// 		cesta.style.left = "400px"
-// 	}
-
-// 	if (posicionX < 0) {
-// 		cesta.style.left = "0px";
-// 	}
-// }
-
-//////////////////
+var generacionBolitas = setInterval(getBolita, 800);
+var movimientoBolitas = setInterval(moverBolitas, 100);
 
 
 document.addEventListener('keydown', moverCesta);
@@ -113,26 +96,129 @@ function moverBolitas() {
 
 		if (bolitaTopPosition > 550 && bolitaTopPosition < 600 &&
 			bolitaLeftPosition + 25 > cestaPositionLeft && bolitaLeftPosition < cestaPositionLeft + 200) {
-
+			
+			cesta.style.backgroundColor = bolita.style.backgroundColor;
 			bolita.remove();
 			score++;
-			console.log(score);
-
-
-
+			scoreDisplay.innerText = score;
 		}
 
 		if (bolita.style.top == "620px") {
 			bolita.remove();
-
 			lifes--;
+			removeLife();
 
 		} 
-
 	}
 }
 
+function removeLife() {
 
-setInterval(moverBolitas, 100);
+	switch(lifes) {
+		case 4:
+			hearts[0].className = "fas fa-heart-broken hearts brokenHearts";
+		break;
 
-setInterval(getBolita, 800);
+		case 3:
+			hearts[1].className = "fas fa-heart-broken hearts brokenHearts";
+		break;
+
+		case 2:
+			hearts[2].className = "fas fa-heart-broken hearts brokenHearts";
+		break;
+
+		case 1:
+			hearts[3].className = "fas fa-heart-broken hearts brokenHearts";
+		break;
+
+		case 0:
+			hearts[4].className = "fas fa-heart-broken hearts brokenHearts";
+			gameOver();
+		break;
+
+	}
+
+}
+
+
+function gameOver() {
+
+	clearInterval(generacionBolitas);
+	clearInterval(movimientoBolitas);
+
+
+	var gameOverDiv = document.createElement('div');
+	gameOverDiv.innerHTML = "<h1 style='font-size: 60px'>GAME OVER</h1>";
+
+	gameOverDiv.setAttribute('style', 'padding: 60px 100px; border: 2px solid black; background-color: lightgreen; color: darkgreen; border-radius: 5%; text-align: center; position: absolute; top: 50%; left: 50%; transform: translate(-50%, -80%)');
+
+
+	var scoreDiv = document.createElement('div');
+	scoreDiv.innerHTML = "<h2 style='font-size: 30px'>Score: " + score + "</h2>";
+
+	scoreDiv.setAttribute('style', 'padding: 15px 25px; border: 2px solid black; background-color: green; color: white; border-radius: 15%; text-align: center; position: absolute; top: 50%; left: 50%; transform: translate(-50%, 20%)');
+
+
+	escenario.appendChild(gameOverDiv);
+	escenario.appendChild(scoreDiv);
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////
+
+//version con el movimiento del raton
+
+// escenario.addEventListener('mousemove', moverCesta);
+
+// function moverCesta(event) {
+
+// 	var posicionX = event.layerX -= (cesta.clientWidth / 2);
+
+// 	cesta.style.left = posicionX + "px";
+
+// 	if (posicionX > (escenario.clientWidth - cesta.clientWidth) - 6) {
+// 		cesta.style.left = "400px"
+// 	}
+
+// 	if (posicionX < 0) {
+// 		cesta.style.left = "0px";
+// 	}
+// }
+
+//////////////////
+
+
+
+
+
+
+
+
+
+//ejemplo de uso de nextSibiling como insertBefore...
+// heartsDiv.insertBefore(brokenHeart, hearts.nextSibling);
+
+
+///la otra forma de las vidas: eiminarlas y crear un nuevo elemento con las clases
+
+// hearts[0].remove();
+	// var brokenHeart = document.createElement('i');
+	// brokenHeart.className = "fas fa-heart-broken brokenHearts";
+	
+	// heartsDiv.appendChild(brokenHeart);
